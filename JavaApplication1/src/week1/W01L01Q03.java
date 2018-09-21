21b20ee80052960bdc1f1ce3b3b9cec8e12c8758
wic180035 <wic180035@MIK115.fsktm.edu>
2018-09-21 11:28:06 +0800

    dah siap separuh bulat

--- JavaApplication1/src/week1/W01L01Q03.java
+++ JavaApplication1/src/week1/W01L01Q03.java
@@ -11,14 +11,14 @@ package week1;
  */
 public class W01L01Q03 {
     public static void main(String[] args) {
-        System.out.println("*********");
-        System.out.println("*       *");
-        System.out.println("*       *    8   *");
+        System.out.println("*********           ***");
+        System.out.println("*       *         *");
         System.out.println("*       *       *");
         System.out.println("*       *       *");
         System.out.println("*       *       *");
         System.out.println("*       *       *");
-        System.out.println("*       *");
-        System.out.println("*********");
+        System.out.println("*       *       *");
+        System.out.println("*       *         *");
+        System.out.println("*********           ***");
     }
 }
