f1b1c382d7b3e8d9ab3b85274e8ee99550ffb651
wic180035 <wic180035@MIK115.fsktm.edu>
2018-09-21 11:24:29 +0800

    ada 8

--- JavaApplication1/src/week1/W01L01Q03.java
+++ JavaApplication1/src/week1/W01L01Q03.java
@@ -13,7 +13,7 @@ public class W01L01Q03 {
     public static void main(String[] args) {
         System.out.println("*********");
         System.out.println("*       *");
-        System.out.println("*       *       *");
+        System.out.println("*       *    8   *");
         System.out.println("*       *       *");
         System.out.println("*       *       *");
         System.out.println("*       *       *");
