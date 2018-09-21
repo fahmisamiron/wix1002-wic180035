a13a5489c0f411aa44e374de0d1cf759e84dd82c
wic180035 <wic180035@MIK115.fsktm.edu>
2018-09-21 11:43:06 +0800

    siap keseluruhan tugasan

---
+++ JavaApplication1/src/week1/W01L01Q04.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week1;
+
+/**
+ *
+ * @author wic180035
+ */
+public class W01L01Q04 {
+    public static void main(String[] args) {
+        System.out.println("Month           Amount");
+        System.out.println("January 2016    2500");
+        System.out.println("February 2016   1600");
+        System.out.println("March 2016      2000");
+        System.out.println("April 2016      2700");
+        System.out.println("May 2016        3200");
+        System.out.println("June 2016       800");
+    }
+}
